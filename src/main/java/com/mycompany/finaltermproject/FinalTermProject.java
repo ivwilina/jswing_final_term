c7949c37226c9b4e95a5acf4adc1b917adb721f2
ivwilina <ivwi3103@gmail.com>
2024-04-01 15:48:03 +0700

    First commit

---
+++ src/main/java/com/mycompany/finaltermproject/FinalTermProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.finaltermproject;
+
+/**
+ *
+ * @author asus
+ */
+public class FinalTermProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
